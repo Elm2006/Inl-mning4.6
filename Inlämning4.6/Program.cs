72608514e8c54fb31534e2203b8daa7164a56e5e
isel06002 <isel06002@E-PW0234HX>
2023-10-20 09:17:31 +0200

    Add project files.

---
+++ Inlämning4.6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Inlämning_4.__6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett meddelande");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("Hu många steg år höger ska meddelandet skrivas");
+            int steg = int.Parse(Console.ReadLine());
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+               for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+    }
+}
